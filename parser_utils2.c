b3135df7923809bd3ba9f842e5ee0b92ce199562
MouadDv <mouaddv@gmail.com>
2021-10-24 06:59:54 +0100

    norm

--- parser_utils2.c
+++ parser_utils2.c
@@ -1,5 +1,33 @@
 #include "minishell.h"
 
+void    sizelen_norm(int    *i, int    *ret, char   *str)
+{
+    int     trig;
+
+    if (str[*i] == '\'')
+    {
+        *ret = *ret + 1;
+        *i = *i + 1;
+        trig = 1;
+        while (str[*i])
+        {
+            if (str[*i] == '\'' && trig == 0)
+                trig = 1;
+            else if (str[*i] == '\'')
+                trig = 0;
+            if (str[*i] == ' ' && trig == 0)
+                break;
+             *i = *i + 1;
+        }
+    }
+    else if (str[*i] != ' ')
+    {
+        *ret = *ret + 1;
+        while (str[*i] && str[*i] != ' ')
+             *i = *i + 1;
+    }
+}
+
 int     sizelen(char    *str)
 {
     int     ret;
@@ -26,31 +54,51 @@ int     sizelen(char    *str)
                 i++;
             }
         }
-        else if (str[i] == '\'')
-        {
-            ret++;
-            i++;
-            trig = 1;
-            while (str[i])
-            {
-                if (str[i] == '\'' && trig == 0)
-                    trig = 1;
-                else if (str[i] == '\'')
-                    trig = 0;
-                if (str[i] == ' ' && trig == 0)
-                    break;
-                i++;
-            }
-        }
-        else if (str[i] != ' ')
-        {
-            ret++;
-            while (str[i] && str[i] != ' ')
-                i++;
-        }
+        else if (str[i] != ' ' || str[i] == '\'')
+            sizelen_norm(&i, &ret, str);
         if (str[i] == '"' || str[i] == '\'' || str[i] == ' ')
             i++;
     }
+    return (ret);
+}
+
+char     *singlequote(char    *str, int   *s, int i)
+{
+    char    *ret;
+    int     trig;
+
+    trig = 1;
+    while (str[*s + i])
+    {
+        if (str[*s + i] == '\'' && trig == 0)
+            trig = 1;
+        else if (str[*s + i] == '\'')
+            trig = 0;
+        if (str[*s + i] == ' ' && trig == 0)
+            break;
+        *s = *s + 1;
+    }
+    ret = ft_substr(str + i, 0, *s);
+    return (ret);
+}
+
+char     *doublequote(char    *str, int   *s, int i)
+{
+    char    *ret;
+    int     trig;
+
+    trig = 1;
+    while (str[*s + i])
+    {
+        if (str[*s + i] == '"' && trig == 0)
+            trig = 1;
+        else if (str[*s + i] == '"')
+            trig = 0;
+        if (str[*s + i] == ' ' && trig == 0)
+            break;
+        *s = *s + 1;
+    }
+    ret = ft_substr(str + i, 0, *s);
     return (ret);
 }
 
@@ -59,42 +107,15 @@ char    *get_arg(char   *str, int   *r)
     int     i;
     char    *ret;
     int     s;
-    int     trig;
 
     i = *r;
     s = 1;
     while (str[i] && str[i] == ' ')
         i++;
     if (str[i] == '\'')
-    {
-        trig = 1;
-        while (str[s + i])
-        {
-            if (str[s + i] == '\'' && trig == 0)
-                trig = 1;
-            else if (str[s + i] == '\'')
-                trig = 0;
-            if (str[s + i] == ' ' && trig == 0)
-                break;
-            s++;
-        }
-        ret = ft_substr(str + i, 0, s);
-    }
+        ret = singlequote(str, &s, i);
     else if (str[i] == '"')
-    {
-        trig = 1;
-        while (str[s + i])
-        {
-            if (str[s + i] == '"' && trig == 0)
-                trig = 1;
-            else if (str[s + i] == '"')
-                trig = 0;
-            if (str[s + i] == ' ' && trig == 0)
-                break;
-            s++;
-        }
-        ret = ft_substr(str + i, 0, s);
-    }
+        ret = doublequote(str, &s, i);
     else
     {
         while (str[s + i] && str[s + i] != ' ')
