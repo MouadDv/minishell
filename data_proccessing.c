772d7037879e254b28fa876c0b528aa1538248d9
MouadDv <mouaddv@gmail.com>
2021-10-28 08:54:46 +0100

    env replace

--- data_proccessing.c
+++ data_proccessing.c
@@ -1,8 +1,42 @@
 #include "minishell.h"
 
+char    *get_name(char  *str)
+{
+    char    *ret;
+    int     i;
+
+    i = 1;
+    while (str[i] && str[i] != '$' && str[i] != '"' && str[i] != ' ')
+        i++;
+    ret = ft_substr(str, 1, i - 1);
+    return (ret);
+}
+
+void    replace_env(char    **str, t_node	*node)
+{
+    char    *locat;
+    char    *name;
+
+    locat = ft_strchr(*str, '$');
+    while (locat != NULL)
+    {
+        if (*(locat + 1) != '"' &&  *(locat + 1) != '$' && *(locat + 1) != '\0' && *(locat + 1) != ' ')
+        {
+            printf("{%s}\n", locat);
+            name = get_name(locat);
+            printf("[%s] = %s\n", name, env_val(name, node));
+        }
+        locat = ft_strchr(locat + 1, '$');   
+    }
+    
+}
+
 int     data_proc(t_cmd   *strct, t_node	*node)
 {
     strct = NULL;
-    node = NULL;
+    char    *str;
+
+    str = strdup("echo $HOME $PATH dsfk \"$HOME\"");
+    replace_env(&str, node);
     return (1);
 }
